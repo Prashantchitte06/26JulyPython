aafc285bd5f4cbf7e6245b1bb0952e4e33af1ae9
Prashantchitte06 <chitteprashant06@gmail.com>
2025-09-11 08:42:01 +0530

    11th Sept 25 String functions

--- StringFunctions/String1.py
+++ StringFunctions/String1.py
@@ -1,7 +1,57 @@
 s1="velocity"
 s2="ABCD"
 s3="abcd"
+s4="abcdabcaba"
+s5=" abcd "
+s6="my name is abc"
 
+s7="  "
+s8="abcd"
+s9="1223"
+s10="abc123"
+
+print(s10.isalnum())
+print(s9.isdigit())
+print(s8.isalpha())
+print(s7.isspace())
+print(s6.count("my"))   #1
+
+#convert 1st to upper case
+print(s3.capitalize())
+#convert 1st letter of each word to upper case
+print(s6.title())
+
+#split statement
+ls=s6.split(" ")     #[my    name    is abc]
+print(ls)
+
+#remove spaces from string
+print(s5)
+print(s5.strip())
+#remove space from left
+print(s5.lstrip())
+#remove space from right
+print(s5.rstrip())
+
+#CONCAT 2 STRINGS
+print(s2+s3)
+
+print("-----------")
+#find index of specific char
+print(s4.index("b"))
+print(s4.find("b"))
+
+#find index of specific char from right side (last occurance of char)
+print(s4.rfind("b"))   #8
+
+
+#find char using index
+print(s1[3])
+#Substring
+print(s1[3:])
+print(s1[3:6])      #3-5
+
+print("-------------")
 print(len(s1))      #8
 
 print(s1.upper())
